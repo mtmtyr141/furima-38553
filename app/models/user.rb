02761929ed2c3e917369423ed227ef19a3623a55
mtmtyr141 <1410190107a@gmail.com>
2022-10-27 11:09:22 +0900

    session ビュー正しい形に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+         validates  :name
+         validates :birthday
+         validates :firstname
+         validates :lastname
+         validates :firstname_kana
+         validates :lastname_kana
+         end
+
+
+         has_many :items
+         has_many :orders
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2022_10_25_110727) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.date "birthday", null: false
+    t.string "firstname", null: false
+    t.string "lastname", null: false
+    t.string "firstname_kana", null: false
+    t.string "lastname_kana", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
