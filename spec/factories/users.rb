0d3d5ccc13a4d80fb17f02106d86a069490cf115
mtmtyr141 <1410190107a@gmail.com>
2022-10-28 19:56:00 +0900

    Factoryファイルの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,16 @@
+
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.initials(number: 2) }
+    name { Faker::Name.initials(number: 2)}
     email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    firstname { '山田'}
+    lastname   { '太郎'}
+    firstname_kana { 'ヤマダ'}
+    lastname_kana { 'タロウ'}
+    birthday { '2000-01-01' }
   end
 end
+
+
