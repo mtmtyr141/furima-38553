e21ac4d4294598f18b2c9391b2a20394d0b2d4a8
mtmtyr141 <1410190107a@gmail.com>
2022-10-29 16:32:05 +0900

    テストの要件の追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe User, type: :model do
 
     context '新規登録できない場合' do
 
-      it "nameが必須であること" do
+      it "nameが空では登録できない" do
         @user.name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Name can't be blank")
       end
     
-      it 'emailが必須であること' do
+      it 'emailが空では登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "Email can't be blank"
@@ -32,18 +32,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
-      it 'passwordは、6文字以下での入力が必須であること' do
+      it 'passwordは、6文字以下では登録できない' do
         @user.password = '1234a'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
-      it 'passwordが半角英数字の混合でなければ登録できない' do
-        @user.password = 'aaaaaa'
-        @user.password_confirmation = 'aaaaaa'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid. Input half-width characters.')
-      end
+      
 
       it 'passwordとpassword_confirmationは値の一致が必須であること' do
         @user.password_confirmation = ''
@@ -81,41 +76,40 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Firstname kana can't be blank")
       end
 
-      it 'lastname_kanaが空だと登録できない' do
+      it 'lastname_kanaが空では登録できない' do
         @user.lastname_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Lastname kana can't be blank")
       end
 
-      it 'firstname_kanaが英数字だと登録できない' do
+      it 'firstname_kanaが英数字では登録できない' do
         @user.firstname_kana = 'aaaaa1'
         @user.valid?
         expect(@user.errors.full_messages).to include("Firstname kana is invalid. Input full-width katakana characters.")
       end
 
-      it 'firstname_kanaが漢字だと登録できない' do
+      it 'firstname_kanaが漢字では登録できない' do
         @user.firstname_kana = '山田'
         @user.valid?
         expect(@user.errors.full_messages).to include("Firstname kana is invalid. Input full-width katakana characters.")
       end
       
-      it 'lastname_kanaが英数字だ登録できない' do
+      it 'lastname_kanaが英数字では登録できない' do
         @user.lastname_kana = 'aaaaa1'
         @user.valid?
         expect(@user.errors.full_messages).to include("Lastname kana is invalid. Input full-width katakana characters.")
       end
-      it 'lastname_kanaが漢字だと登録できない' do
+      it 'lastname_kanaが漢字では登録できない' do
         @user.lastname_kana = '太郎'
         @user.valid?
         expect(@user.errors.full_messages).to include("Lastname kana is invalid. Input full-width katakana characters.")
       end
 
-      it 'birthdayが空だと登録できない' do
+      it 'birthdayが空では登録できない' do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
-
 
       it 'emailは一意性であること' do
         @user.save
@@ -125,6 +119,26 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
+      it 'passwordは英語のみでは登録できないこと' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Input half-width characters.')
+      end
+
+      it 'passwordは数字のみでは登録できないこと' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Input half-width characters.')
+      end
+
+      it 'passwordは全角では登録できないこと' do
+        @user.password = 'aaa１１１'
+        @user.password_confirmation = 'aaa１１１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Input half-width characters.')
+      end
 
 
 
