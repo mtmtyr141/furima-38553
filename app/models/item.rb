bf113e909623006316e9957d2223ae39d1ddf3cf
mtmtyr141 <1410190107a@gmail.com>
2022-10-31 12:20:46 +0900

    itemモデル バリテーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :price
+    validates :detail
+    validates :state-id
+    validates :area_id
+    validates :category_id
+    validates :day_id
+    validates :burden_id
+
+
 end
