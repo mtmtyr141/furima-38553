bee34bc896eb9aa70ca54ceb67a71debb2eb39fa
mtmtyr141 <1410190107a@gmail.com>
2022-10-31 18:09:54 +0900

    Active Storageの導入

--- db/migrate/20221023123744_create_items.rb
+++ db/migrate/20221023123744_create_items.rb
@@ -2,7 +2,21 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
+      t.string     :name,          null: false
+      t.integer    :price,         null: false
+      t.references :user,          null: false,foreign_key: true
+      t.text       :detail,        null: false
+      t.integer    :state_id,      null: false
+      t.integer    :area_id,       null: false
+      t.integer    :category_id,   null: false
+      t.integer    :day_id,        null: false
+      t.integer    :burden_id,     null: false
+
       t.timestamps
     end
   end
 end
+
+
+
+
