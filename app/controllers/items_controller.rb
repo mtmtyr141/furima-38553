f55b0000f9637d6d2c3b7c9092d49284eab889a2
mtmtyr141 <1410190107a@gmail.com>
2022-10-31 12:22:40 +0900

    コントローラー 出品機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,24 @@ class ItemsController < ApplicationController
   end
 
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  def item_params
+    params.require(:item).permit(:image, :name, :price, :detail, :state_id, :area_id, :category_id, :day_id, :burden_id).merge(user_id: current_user.id)
+  end
+
+
+
+
+
+
+
 end
