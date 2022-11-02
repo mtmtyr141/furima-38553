94a20a5526e3c2ccc5d060576c7a2344fcb4a12a
mtmtyr141 <1410190107a@gmail.com>
2022-11-02 14:29:57 +0900

    バリテーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,15 +15,15 @@ class Item < ApplicationRecord
   
 
 
-    validates :state_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :state_id, numericality: { other_than: 1, message: "can't be blank" }
 
-    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
-    validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 
-    validates :day_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :day_id, numericality: { other_than: 1, message: "can't be blank" }
     
-    validates :burden_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :burden_id, numericality: { other_than: 1, message: "can't be blank" }
 
 
 
