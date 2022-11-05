68f7f7d8af7702895e5a341dc31140b139e3fa50
mtmtyr141 <1410190107a@gmail.com>
2022-11-05 23:25:23 +0900

    orders コントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :set_limits, only: [:index, :create]
+
+
+
 
   def index
     @order_address = OrderAddress.new
@@ -9,6 +13,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+       pay_item
        @order_address.save
        redirect_to root_path
     else
@@ -16,14 +21,34 @@ class OrdersController < ApplicationController
     end
   end
 
+  private
 
   def order_params
-    params.require(:order_address).permit(:postal, :area_id, :city, :block, :building, :number, :order).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal, :area_id, :city, :block, :building, :phonenumber, :order).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Item.find(params[:item_id]).price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def set_limits
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
+    unless @item.order == nil
+      redirect_to root_path
+    end
   end
  
 end
