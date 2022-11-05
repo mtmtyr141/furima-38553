e0722ba928d93065f2f87bd1655b90ee4a9abce4
mtmtyr141 <1410190107a@gmail.com>
2022-11-05 23:25:07 +0900

    orders ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
+  root to: "items#index"
   resources :items do
-    resources :orders, only: %i[create index]
+    resources :orders, only: [:index, :create]
   end
-  resources :users, only: [:show]
 end
